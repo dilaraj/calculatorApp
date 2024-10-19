298e70a4066903015fccc4e5d01777cfa9467494
Dilara Ajaj <106478364+dilaraj@users.noreply.github.com>
2024-10-19 12:23:44 +0000

    Creating basic math functions

---
+++ calculator.js
@@ -0,0 +1,21 @@
+// Creating basic math operation functions
+let add = (n1, n2) => {
+    return n1 + n2;
+}
+
+let subtract = (n1, n2) => {
+    return n1 - n2;
+}
+
+let multiply = (n1, n2) => {
+    return n1 * n2;
+}
+
+let divide = (n1, n2) => {
+    return n1 / n2;
+}
+
+// console.log(add(1,2))
+// console.log(subtract(1,2))
+// console.log(multiply(1,2))
+// console.log(divide(1,2))
